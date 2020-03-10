49e180319569a79d07daef7a90f2d1253233183a
Umka3250 <joker1233214@gmail.com>
2020-03-10 16:00:18 +0300

    Create build of project

---
+++ build/precache-manifest.449e33d1e5066f497cc04ce8823080ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090fceb98b713203de0ce48bbc09d065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe5b6762b79fbe494c4f",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "35b588898b8c7563ec50",
+    "url": "/static/css/main.97124139.chunk.css"
+  },
+  {
+    "revision": "fe5b6762b79fbe494c4f",
+    "url": "/static/js/2.d53a381f.chunk.js"
+  },
+  {
+    "revision": "5aa0ee45bd75d4e6390506b313cd17bd",
+    "url": "/static/js/2.d53a381f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b588898b8c7563ec50",
+    "url": "/static/js/main.09e4f6ac.chunk.js"
+  },
+  {
+    "revision": "c4a7f7998775d9b7351d",
+    "url": "/static/js/runtime-main.a4bd9d66.js"
+  }
+]);
