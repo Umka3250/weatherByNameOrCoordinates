97d0bebe2533171854d4f565ed52fc8b14c37aff
Umka3250 <joker1233214@gmail.com>
2020-03-13 16:02:32 +0300

    Updates

---
+++ precache-manifest.616fe7dfeb55945c7c7750ed081f7f4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307908e2bbd141d75e0c20be25de0826",
+    "url": "/weatherByNameOrCoordinates/index.html"
+  },
+  {
+    "revision": "3cac080e0f01d0abb17c",
+    "url": "/weatherByNameOrCoordinates/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7006021b898ed6169c41",
+    "url": "/weatherByNameOrCoordinates/static/css/main.9f117828.chunk.css"
+  },
+  {
+    "revision": "3cac080e0f01d0abb17c",
+    "url": "/weatherByNameOrCoordinates/static/js/2.975e13a4.chunk.js"
+  },
+  {
+    "revision": "5aa0ee45bd75d4e6390506b313cd17bd",
+    "url": "/weatherByNameOrCoordinates/static/js/2.975e13a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7006021b898ed6169c41",
+    "url": "/weatherByNameOrCoordinates/static/js/main.c3b98f7e.chunk.js"
+  },
+  {
+    "revision": "105d003fdcf1b206f03e",
+    "url": "/weatherByNameOrCoordinates/static/js/runtime-main.e47ad85d.js"
+  }
+]);
