7afa25b65ac360779f5d14b22bf890926a608353
Umka3250 <joker1233214@gmail.com>
2020-03-13 17:05:05 +0300

    Updates

---
+++ precache-manifest.99368551a388dd11567cd436bd020a0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe601296d385b12185abf88bde482ed",
+    "url": "/weatherByNameOrCoordinates/index.html"
+  },
+  {
+    "revision": "1ae7abf217037aea8629",
+    "url": "/weatherByNameOrCoordinates/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1fa700afa3b9a10e39fe",
+    "url": "/weatherByNameOrCoordinates/static/css/main.76187c10.chunk.css"
+  },
+  {
+    "revision": "1ae7abf217037aea8629",
+    "url": "/weatherByNameOrCoordinates/static/js/2.2db3eb2e.chunk.js"
+  },
+  {
+    "revision": "5aa0ee45bd75d4e6390506b313cd17bd",
+    "url": "/weatherByNameOrCoordinates/static/js/2.2db3eb2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa700afa3b9a10e39fe",
+    "url": "/weatherByNameOrCoordinates/static/js/main.e1d4db80.chunk.js"
+  },
+  {
+    "revision": "105d003fdcf1b206f03e",
+    "url": "/weatherByNameOrCoordinates/static/js/runtime-main.e47ad85d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weatherByNameOrCoordinates/precache-manifest.616fe7dfeb55945c7c7750ed081f7f4c.js"
+  "/weatherByNameOrCoordinates/precache-manifest.99368551a388dd11567cd436bd020a0c.js"
 );
 
 self.addEventListener('message', (event) => {
